6dfefecff18b0b45062d4eae3784117834a62bbf
Alex Choi <sequoia.alex.ca@gmail.com>
2024-05-15 15:02:15 -0700

    Added email sending feature for password reset.

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const mongodb_dt_sessions = process.env.MONGODB_DATABASE_SESSION;
 const mailjet = require('node-mailjet').apiConnect(
     process.env.MJ_APIKEY_PUBLIC,
     process.env.MJ_APIKEY_PRIVATE
-  );
+);
 
 const expireTime = 1 * 60 * 60 * 1000;
 
@@ -29,12 +29,10 @@ const MongoDBSessionURI = `mongodb+srv://${mongodb_user}:${mongodb_password}@${m
 
 const userModel = require("./user.js");
 
-mongoose.connect(MongoURI, {
-})
-    .then(res => {
+mongoose.connect(MongoURI, {}).then(res => {
         console.log('MongoDB Connected');
-    })
-    
+})
+
 app.set('view engine', 'ejs');
 
 const mongoStore = connectMongo.create({
@@ -46,8 +44,7 @@ const mongoStore = connectMongo.create({
 
 app.use(express.urlencoded({ extended: false }));
 
-app.use(
-    session({
+app.use(session({
         secret: node_session_secret,
         saveUninitialized: false,
         resave: true,
@@ -55,8 +52,7 @@ app.use(
         cookie: {
             maxAge: expireTime
         }
-    }
-    ));
+}));
 
 function isValidSession(req) {
     if (!req.session.authenticated) {
@@ -76,32 +72,153 @@ function sessionValidation(req, res, next) {
     }
 };
 
+function generateRandomPassword(length) {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+[]{}|;:,.<>?';
+    let password = '';
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * chars.length);
+        password += chars[randomIndex];
+    }
+
+    return password;
+}
+
+// const sendEmail = (recipientEmail, recipientName, templateId, templateVariables) => {
+//     const request = mailjet.post('send', { version: 'v3.1' }).request({
+//         Messages: [
+//             {
+//                 From: {
+//                     Email: 'bby01.290124@gmail.com',
+//                     Name: 'LearnXchange',
+//                 },
+//                 To: [
+//                     {
+//                         Email: recipientEmail,
+//                         Name: recipientName,
+//                     },
+//                 ],
+//                 Subject: 'Test Email from Mailjet and Node.js',
+//                 TemplateID: templateId,
+//                 Variables: templateVariables,
+//                 TemplateLanguage: true,
+//             },
+//         ],
+//     });
+//     request.then((result) => {
+//         console.log('Email sent successfully:', JSON.stringify(result.body, null, 2));
+//     }).catch((err) => {
+//         console.error('Error sending email:', JSON.stringify(err, null, 2));
+//     });
+// };
+
+
+
 // app.post('/login/:type', async (req, res) => {
 //     const forgorType = req.params.type;
 //     await 
 //     res.redirect('/login', { forgorType });
 // });
 
+
+
 app.get('/login', (req, res) => {
     var forgor = req.query.type || 'know';
-    console.log(forgor);
-    res.render('login', {forgor});
-    }
-);
+    console.log('forgor type' + forgor);
+    res.render('login', { forgor });
+});
 
-app.post('/resetConfirm', (req,res) => {
-    res.render('resetConfirm');
+app.post('/resetConfirm', async (req, res) => {
+    try {
+        const email = req.body.email;
+        const forgor = req.query.type;
+        const schema = Joi.string().email().max(30).required();
+        const validationResult = schema.validate(email);
+        if (validationResult.error != null) {
+            console.log(validationResult.error);
+            res.render('login', { forgor, errorMessage: "Invalid email." });
+            return;
+        }
+
+        const result = await userModel.findOne({ email });
+        console.log('User info from db'+ result);
+        if (!result) {
+            console.log("user not found");
+            res.render('login', { forgor, errorMessage: 'No user detected.' });
+            return;
+        }
+
+        var emailName = ''
+        let i = 0
+        while (i < result.email.length) {
+            if (result.email[i] == '@') {
+                break;
+            }
+            emailName += result.email[i];
+            i++
+        }
+
+        var newPW = generateRandomPassword(10);
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(newPW, saltRounds);
+
+        const vari = 
+            {
+                link: `https://2800-202410-bby01.onrender.com/newPassword?${emailName}`,
+                newPW: newPW,
+            };
+
+        await userModel.updateOne({ email: email }, { $set: { password: hashedPassword } });
+        console.log('hashed Password' + hashedPassword)
+        const request = mailjet.post('send', { version: 'v3.1' }).request({
+            Messages: [
+                {
+                    From: {
+                        Email: 'bby01.290124@gmail.com',
+                        Name: 'LearnXchange',
+                    },
+                    To: [
+                        {
+                            Email: result.email,
+                            Name: result.name,
+                        },
+                    ],
+                    Subject: `Password reset`,
+                    TextPart: `Your new password is ${newPW}`,
+                    TemplateID: 5969125,
+                    Variables: vari
+                },
+            ],
+        });
+        request.then((result) => {
+            console.log('Email sent successfully:', JSON.stringify(result.body, null, 2));
+        }).catch((err) => {
+            console.error('Error sending email:', JSON.stringify(err, null, 2));
+        });
+        // sendEmail(
+        //     result.email,
+        //     result.name,
+        //     5969125,
+
+        // );
+        res.render('resetConfirm');
+    } catch (error) {
+        console.error('Error in resetConfirm:', error);
+        res.render('login', { forgor, errorMessage: 'An error occurred while processing your request.' });
+    }
+
+    
 });
 
 app.get('/signup', (req, res) => {
     res.render('signup');
-    }
+}
 );
 
 // app.use('/');
 app.get('/', (req, res) => {
     res.render('/');
-    } 
+}
 );
 
 app.post('/signupSubmit', async (req, res) => {
@@ -125,16 +242,13 @@ app.post('/signupSubmit', async (req, res) => {
         return; */
 
     } else {
-
         let user = await userModel.findOne({ email });
-
         if (user) {
             res.redirect('/signup');
             return;
         }
 
         const hashedPass = await bcrypt.hash(password, 12);
-
         user = new userModel({
             name,
             email,
@@ -164,7 +278,7 @@ app.post('/loginSubmit', async (req, res) => {
     }
     const result = await userModel.find({ email: email }).exec();
 
-    console.log(result);
+    console.log('User info from DB' + result);
     if (result.length != 1) {
         console.log("user not found");
         res.render("loginSubmit", { errorMessage: "No User Detected" });
@@ -188,80 +302,43 @@ app.post('/loginSubmit', async (req, res) => {
         res.render("loginSubmit", { errorMessage: "Incorrect Password" });
         return;
     }
-
 });
 
+// async function sendEmail(name, email, subject, message) {
+//     const data = JSON.stringify({
+//       "Messages": [{
+//         "From": {"Email": "bby01.290124@gmail.com", "Name": "LearnXchange"},
+//         "To": [{"Email": email, "Name": name}],
+//         "Subject": subject,
+//         "TextPart": message
+//       }]
+//     });
 
+//     const config = {
+//       method: 'post',
+//       url: 'https://api.mailjet.com/v3.1/send',
+//       data: data,
+//       headers: {'Content-Type': 'application/json'},
+//       auth: {username: process.env.MJ_APIKEY_PUBLIC , password: process.env.MJ_APIKEY_PRIVATE},
+//     };
 
+//     return axios(config)
+//       .then(function (response) {
+//         console.log(JSON.stringify(response.data));
+//       })
+//       .catch(function (error) {
+//         console.log(error);
+//       });
 
-const request = mailjet
-  .post('send', { version: 'v3.1' })
-  .request({
-    Messages: [
-      {
-        From: {
-          Email: 'your@email.com',
-          Name: 'Your Name',
-        },
-        To: [
-          {
-            Email: 'recipient@email.com',
-            Name: 'Recipient Name',
-          },
-        ],
-        Subject: 'Test Email from Mailjet and Node.js',
-        TemplateID: templateId,
-        Variables:{
-        //variables you want to use in your templete
-          name:"test"
-          },
-       TemplateLanguage: true,
-      },
-    ],
-  });
-request
-  .then((result) => {
-    console.log(result.body);
-  })
-  .catch((err) => {
-    console.error(err.statusCode, err.message);
-  });
+//   }
 
-/* async function sendEmail(name, email, subject, message) {
-    const data = JSON.stringify({
-      "Messages": [{
-        "From": {"Email": "bby01.290124@gmail.com", "Name": "LearnXchange"},
-        "To": [{"Email": email, "Name": name}],
-        "Subject": subject,
-        "TextPart": message
-      }]
-    });
-  
-    const config = {
-      method: 'post',
-      url: 'https://api.mailjet.com/v3.1/send',
-      data: data,
-      headers: {'Content-Type': 'application/json'},
-      auth: {username: '<API Key>', password: '<Secret Key>'},
-    };
-  
-    return axios(config)
-      .then(function (response) {
-        console.log(JSON.stringify(response.data));
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  
-  }
-  
-  // define your own email api which points to your server.
-  app.post('/api/sendemail/', function (req, res) {
-    const {name, email, subject, message} = req.body;
+// define your own email api which points to your server.
+app.post('/api/sendemail/', function (req, res) {
+    const { name, email, subject, message } = req.body;
     //implement your spam protection or checks.
     sendEmail(name, email, subject, message);
-  }); */
+});
 
 app.listen(port, () => {
-    console.log('Server is running on port ${port}');
+    console.log(`Server is running on port ${port}`);
 });
