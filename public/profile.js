4e8d018b9a700423786da93c68a81f188b43d6aa
AlfredoLuzardo <luzardoneto2005@gmail.com>
2024-05-23 22:25:27 -0700

    Completed majority of CSS for the profile page. Displays preview of image that you chose in the image selection screen, and created the mobile view of profile page.

--- public/profile.js
+++ public/profile.js
@@ -1,6 +1,27 @@
 const showImagePopup = document.getElementById('profileImage');
 const popupContainer = document.querySelector('.settingsDiv');
+const exitIcon = document.getElementById('exitIcon');
+var imageDiv = document.getElementById('editImagePicture');
+let originalImageSource = '<script type="text/javascript">document.write($.cloudinary.imageTag(\'<%- user.image_id %>\', { height: 100, width: 100, crop: "fit" }).toHtml());</script>';
 
 showImagePopup.onclick = () => {
     popupContainer.classList.add('active');
 };
+
+exitIcon.onclick = () => {
+    popupContainer.classList.remove('active');
+    // imageDiv.innerHTML = '';
+    // let scriptElement = document.createElement('script');
+    // scriptElement.innerHTML = originalImageSource;
+    // imageDiv.appendChild(scriptElement);
+};
+
+function previewImage(event) {
+    imageDiv.innerHTML = '';
+    var img = document.createElement('img');
+    img.src = URL.createObjectURL(event.target.files[0]);
+    img.height = 100;
+    img.width = 100;
+    img.style.objectFit = 'cover';
+    imageDiv.appendChild(img);
+}
