35fa52844584794baff7eaa82d546d2c0a1e69e6
Wayne Wong <wwong312@my.bcit.ca>
2024-05-25 20:38:22 -0700

    cleanup of code, commenting out jquery, moving css to separate css files, update css for forms - profile.js, styles.css, about.ejs, index.ejs, notifications.ejs, profile.ejs, settings.ejs, signup.ejs, footer_after_login.ejs, footer_before_login.ejs, forgorLogin.ejs, forgorType.ejs, header.ejs, matchedUser.ejs, normalLogin.ejs

--- public/profile.js
+++ public/profile.js
@@ -4,17 +4,35 @@ const exitIcon = document.getElementById('exitIcon');
 var imageDiv = document.getElementById('defaultProfilePicture');
 let originalImageSource = '<script type="text/javascript">document.write($.cloudinary.imageTag(\'<%- user.image_id %>\', { height: 100, width: 100, crop: "fit" }).toHtml());</script>';
 
-showImagePopup.onclick = () => {
-    popupContainer.classList.add('active');
-};
+// showImagePopup.onclick = () => {
+//     popupContainer.classList.add('active');
+// };
 
-exitIcon.onclick = () => {
+if (!showImagePopup) {
+  // Handle the case where showImagePopup is null
+  // console.error("showImagePopup is null!");
+} else {
+  showImagePopup.onclick = () => {
+    popupContainer.classList.add('active');
+  };
+}
+
+// exitIcon.onclick = () => {
+//     popupContainer.classList.remove('active');
+//     // imageDiv.innerHTML = '';
+//     // let scriptElement = document.createElement('script');
+//     // scriptElement.innerHTML = originalImageSource;
+//     // imageDiv.appendChild(scriptElement);
+// };
+
+if (!exitIcon) {
+  // Handle the case where exitIcon is null
+  // console.error("exitIcon is null!");
+} else {
+  exitIcon.onclick = () => {
     popupContainer.classList.remove('active');
-    // imageDiv.innerHTML = '';
-    // let scriptElement = document.createElement('script');
-    // scriptElement.innerHTML = originalImageSource;
-    // imageDiv.appendChild(scriptElement);
-};
+  };
+}
 
 function previewImage(event) {
     imageDiv.innerHTML = '';
